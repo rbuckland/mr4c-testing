c90f755873ca7cd92b770ef602cad4bc1479cda2
Ramon Buckland <ramon@thebuckland.com>
2015-10-10 09:16:45 -0400

    basic properties output dumperwq

--- src/cpp/Mr4cProperties.cpp
+++ src/cpp/Mr4cProperties.cpp
@@ -1,32 +1,49 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <stdlib.h>
 
 // mr4c include
 #include <algo_dev_api.h>
+#include <util/util_api.h>
 
 using namespace MR4C;
 
 class Mr4cProperties : public Algorithm {
 
 private:
-    AlgorithmData& data;
-    AlgorithmContext& context;
+//    AlgorithmData data;
+//    AlgorithmContext context;
 
 public:
+ 
+    Mr4cProperties() {};
+
     void executeAlgorithm(AlgorithmData& data, AlgorithmContext& context)
     {
 
-        this.data = data;
-        this.context = context;
+//        this.data = data;
+ //     this.context = context;
 
-        std::map<std::string,std::string> properties =
-          MR4CEnvironment.instance().getProperties(MR4CEnvironment.EnvSet.RAW).getProperties();
+        dumpProperties(MR4CEnvironment::RUNTIME,"RUNTIME");
+        dumpProperties(MR4CEnvironment::JAVA,"JAVA");
+        dumpProperties(MR4CEnvironment::CUSTOM,"CUSTOM");
+        dumpProperties(MR4CEnvironment::RAW,"RAW");
+    }
 
+    void dumpProperties(MR4CEnvironment::EnvSet envType, std::string envname) {
+
+        std::map<std::string,std::string> properties = MR4CEnvironment::instance().getProperties(envType).getPropertyMap();
+
+        std::cout << "***************** OUT " << envname << "*****************" << std::endl;
         for(std::map<std::string,std::string>::iterator iter = properties.begin(); iter != properties.end(); ++iter)
         {
           std::string k =  iter->first;
           std::string v = iter->second;
-
-          cout << k << " -> " << v << std:endl;
+          std::cout << k << "=" << v << std::endl;
         }
+        std::cout << "***************** END " << envname << " *****************" << std::endl;
+
     }
 
     /*
@@ -34,8 +51,8 @@ public:
      * list input and output datasets here
      * static
      */
-    MR4C::Algorithm* create() {
-        Mr4cProperties * algo = new Mr4cProperties();
+    static MR4C::Algorithm* create() {
+        Mr4cProperties* algo = new Mr4cProperties();
         return algo;
     }
 };
