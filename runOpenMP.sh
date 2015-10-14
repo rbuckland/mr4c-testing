afe931947f2cdd6563e44931314048fb7f54e158
Ramon Buckland <ramon@thebuckland.com>
2015-10-14 07:17:29 -0400

    merged openmp

---
+++ runOpenMP.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export MR4C_SITE=./site.json;
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$PWD/lib
+
+set -e
+
+mr4c_hadoop_remote ./openmp.json -Htasks=1 -Hcores.min=1 -Hcores.max=2 -Hmemory.min=1024 -Hmemory.max=2048
